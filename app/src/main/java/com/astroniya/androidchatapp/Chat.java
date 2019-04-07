114ece9f39961aed407338f9f542ab94790ce2b2
Rahul Gupta <rahulguptagzb09@gmail.com>
2019-04-07 15:13:27 +0530

    First commit

---
+++ app/src/main/java/com/astroniya/androidchatapp/Chat.java
@@ -0,0 +1,109 @@
+package com.astroniya.androidchatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Chat extends AppCompatActivity {
+    LinearLayout layout;
+    ImageView sendButton;
+    EditText messageArea;
+    ScrollView scrollView;
+    Firebase reference1, reference2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        layout = findViewById(R.id.layout1);
+        sendButton = findViewById(R.id.sendButton);
+        messageArea = findViewById(R.id.messageArea);
+        scrollView = findViewById(R.id.scrollView);
+
+        Firebase.setAndroidContext(this);
+        reference1 = new Firebase("#################" + UserDetails.username + "_" + UserDetails.chatWith);
+        reference2 = new Firebase("#############" + UserDetails.chatWith + "_" + UserDetails.username);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String messageText = messageArea.getText().toString();
+
+                if (!messageText.equals("")) {
+                    Map<String, String> map = new HashMap<>();
+                    map.put("message", messageText);
+                    map.put("user", UserDetails.username);
+                    reference1.push().setValue(map);
+                    reference2.push().setValue(map);
+                }
+            }
+        });
+
+        reference1.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                Map map = dataSnapshot.getValue(Map.class);
+                String message = map.get("message").toString();
+                String userName = map.get("user").toString();
+
+                if (userName.equals(UserDetails.username)) {
+                    addMessageBox("You:-\n" + message, 1);
+                } else {
+                    addMessageBox(UserDetails.chatWith + ":-\n" + message, 2);
+                }
+            }
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+    }
+
+    public void addMessageBox(String message, int type) {
+        TextView textView = new TextView(Chat.this);
+        textView.setText(message);
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, 0, 0, 10);
+        textView.setLayoutParams(lp);
+
+        if (type == 1) {
+            textView.setBackgroundResource(R.drawable.rounded_corner1);
+        } else {
+            textView.setBackgroundResource(R.drawable.rounded_corner2);
+        }
+
+        layout.addView(textView);
+        scrollView.fullScroll(View.FOCUS_DOWN);
+    }
+}
---
+++ app/src/main/java/com/astroniya/androidchatapp/UserDetails.java
@@ -0,0 +1,7 @@
+package com.astroniya.androidchatapp;
+
+public class UserDetails {
+    static String username = "";
+    static String password = "";
+    static String chatWith = "";
+}
